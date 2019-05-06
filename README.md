36a5feb5dd906f0cd40f55c14b32ffbe6ccd7f44
Jim Holmberg <hasenpfad1@gmail.com>
2019-05-05 19:30:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# liri-node-app
+# liri-node-app UPDATED 5/5 TO USE SPOTIFY-THIS-SONG
--- liri.js
+++ liri.js
@@ -2,10 +2,14 @@ var axios = require("axios");
 require("dotenv").config();
 var moment = require("./moment.js");
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+//console.log("spotify is " + keys.spotify);
+//console.log("spotify.id is " + keys.spotify.id);
+//console.log("spotify.secret is " + keys.spotify.secret);
 
 var choice = process.argv[2];
-var identifier = process.argv.slice(3).join(" ")
+var identifier = process.argv.slice(3).join(" ");
 
 switch (choice) {
     case "concert-this":
@@ -49,22 +53,41 @@ function movieThis(){
       console.log("Concert date is " + moment(concertDate).format("MMMM Do YYYY"));
       console.log("");
       }
-    /*
-function spotifySong(){
-    axios.get("https://rest.bandsintown.com/artists/" + identifier + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    for (var i = 0; i < response.data.length; i++){
-    console.log(response.data[i].venue.name);
-    console.log(response.data[i].venue.city +", "+ response.data[i].venue.country);
-    var concertDate = response.data[i].datetime;
-    console.log("Concert date is " + moment(concertDate).format("MMMM Do YYYY"));
-    console.log("");
     }
-
-    */
-
+      )
   }
-)
+function spotifySong(){
+       spotify.search({ type:'track', query: identifier}).then(function(response)
+       {var responseData = response.tracks.items;
+        //console.log("JSON OBJECT IS " + JSON.stringify(responseData[0]));
+        dataArray =["The artist is " + responseData[0].artists[0].name,
+        " The title is "+ responseData[0].name,
+        " The album name is "+ responseData[0].album.name,
+        " The title is "+ responseData[0].name,
+        " The preview Link: " + responseData[0].preview_url,
+       ].join("\n\n");
+
+        console.log(dataArray);
+       });
+
+/*.catch(function(error) {
+  console.log("keys.id is "+keys.spotify.id);
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log("Error", error.message);
+  }
+  console.log(error.config);
+});*/
 }
 
 
